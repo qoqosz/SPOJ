98317e6ee7545ba37272f90c7b9d1069cc0dbd02
Łukasz Bednarski <qoqosz@users.noreply.github.com>
2014-05-06 13:08:36 +0200

    Create ACPC10A.cpp

---
+++ ACPC10A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	while (true)
+	{
+		int a, b, c;
+		cin >> a >> b >> c;
+
+		if (a == 0 && b == 0 && c == 0)
+			break;
+
+		if (b-a == c-b)
+			cout << "AP " << 2*c - b << "\n";
+		else
+			cout << "GP " << c*c/b << "\n";
+	}
+
+	return 0;
+}
